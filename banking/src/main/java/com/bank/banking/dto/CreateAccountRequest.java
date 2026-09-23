71ceca5674922ae6c8e2a9958174ddb9229c3d65
Dev Ronooo <rajabki7233@gmail.com>
2026-09-23 18:04:22 +0300

    added this feature

--- banking/src/main/java/com/bank/banking/dto/CreateAccountRequest.java
+++ banking/src/main/java/com/bank/banking/dto/CreateAccountRequest.java
@@ -1,7 +1,12 @@
 package com.bank.banking.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class CreateAccountRequest {
 
+    @NotBlank(message = "Owner name is required")
+    @Size(min = 2, max = 50, message = "Owner name must be 2-50 characters")
     private String ownerName;
 
     public String getOwnerName() { return ownerName; }
